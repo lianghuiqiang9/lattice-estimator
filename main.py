406bd1391af5a048f185cca4bc71de921dda49e6
lianghuiqiang9 <lianghuiqiang92@gmail.com>
2025-04-15 07:26:18 +0000

    test simplepir params

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from estimator import *
-schemes.Kyber512
-LWE.primal_usvp(schemes.Kyber512)
-r = LWE.estimate.rough(schemes.Kyber512)
-r = LWE.estimate(schemes.Kyber512)
 
-schemes.Falcon512_SKR
-r = NTRU.estimate.rough(schemes.Falcon512_SKR)
-r = NTRU.estimate(schemes.Falcon512_SKR)
+params = LWE.Parameters(n=1024, q=2**32, Xs=ND.DiscreteGaussian(6.4), Xe=ND.DiscreteGaussian(6.4), m=2**25)
+print(params)
+
+LWE.estimate(params) #执行全部评估，启用多线程
+"""LWE.estimate.rough(params) #遵从某些假设和启发式，粗略估计"""
+
+"然后再选择p，满足最少\sqrt(N)次的加法，失败概率不高于\delta"
+"失败概率在哪里呢"
+
+
+"测试结果表明，这个基本上保证了120比特的安全，没有达到128比特安全吧"
